061bae5e00199dc04553f0f48d225dc7205ec3f1
Diego Alejandro <diego.fc.1@hotmail.com>
2019-02-08 21:34:21 -0500

    form login full coded

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,6 +45,12 @@ public class HomeController {
 	public String goHome(){
 		
 		return "home";
+	}
+	
+	@GetMapping("/formLogin")
+	public String login () {
+		return "formLogin";
+		
 	}
 	
 	
